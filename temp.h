65154afd5c0b5baa26771301bee3ddd4722f8b1f
Aleksandar Naidenov <za6opz@Aleksandar-Naidenovs-MacBook-Pro.local>
2014-02-25 19:15:13 +0200

    workinng code

---
+++ temp.h
@@ -0,0 +1,21 @@
+/*
+ * temp.h
+ *
+ *  Created on: Feb 20, 2014
+ *      Author: za6opz
+ */
+
+
+uint8_t therm_reset();
+
+void therm_write_bit(uint8_t);
+
+uint8_t therm_read_bit(void);
+
+uint8_t therm_read_byte(void);
+
+void therm_write_byte(uint8_t);
+
+int therm_read_temperature(char*);
+
+
