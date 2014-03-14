7587e896af6cec1303a8d22ed5506723e4c59bb0
Aleksandar Naidenov <anaidenov@mm-sol.com>
2014-03-14 11:20:16 +0200

    fix timer value, it was less than a sec

--- timer.c
+++ timer.c
@@ -8,9 +8,13 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+/*
+ * Target Timer Count = (((Input Frequency / Prescaler) / Target Frequency) - 1)
+ */
+
 void timer_config(void){
 	 TCCR1B = (1<<WGM12)|(1<<CS12); // enable CTS mode; 1HZ=61499 clocks=256 prescale
-	 OCR1A =  61499;            //sets the desired count
+	 OCR1A =  62499;            //sets the desired count
 	 TIMSK1 = (1<<OCIE1A); 		//Enable timer interrupts
 	 sei();    //Enable global interrupts, so our interrupt service routine can be called
 }
